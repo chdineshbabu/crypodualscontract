1975ea6d62467fec847638c26472109ecf10790d
chdineshbabu <chdinesh4128@gmail.com>
2025-02-19 05:37:51 +0530

    Fixed swap contract

---
+++ contracts/Price.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IPrice {
+    function getPoolTokens(bytes32 poolId)
+        external
+        view
+        returns (
+            IERC20[] memory tokens,
+            uint256[] memory balances,
+            uint256 lastChangeBlock
+        );
+}
+
+contract SwapQuoteQuery {
+    IPrice public vault;
+    address public constant HONEY = 0xFCBD14DC51f0A4d49d5E53C2E0950e0bC26d0Dce;
+
+    constructor(address _vaultAddress) {
+        vault = IPrice(_vaultAddress);
+    }
+
+    function getPoolDetails(bytes32 poolId) 
+        external 
+        view 
+        returns (
+            IERC20[] memory tokens,
+            uint256[] memory balances,
+            uint256 lastChangeBlock
+        ) 
+    {
+        return vault.getPoolTokens(poolId);
+    }
+    function getPriceForToken(bytes32 poolId, address token) 
+        external 
+        view 
+        returns (uint256) 
+    {
+        (IERC20[] memory tokens, uint256[] memory balances, ) = vault.getPoolTokens(poolId);
+        int256 indexHoney = -1;
+        int256 indexToken = -1;
+
+        for (uint256 i = 0; i < tokens.length; i++) {
+            if (address(tokens[i]) == HONEY) {
+                indexHoney = int256(i);
+            }
+            if (address(tokens[i]) == token) {
+                indexToken = int256(i);
+            }
+        }
+
+        require(indexHoney >= 0, "HONEY token not found in pool");
+        require(indexToken >= 0, "Provided token not found in pool");
+
+        uint256 balanceHoney = balances[uint256(indexHoney)];
+        uint256 balanceToken = balances[uint256(indexToken)];
+        require(balanceHoney > 0, "Insufficient balance for HONEY");
+        require(balanceToken > 0, "Insufficient balance for token");
+
+        return balanceToken * 1e18 / balanceHoney;
+    }
+}
--- contracts/newSwap.sol
+++ contracts/newSwap.sol
@@ -2,14 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+interface IWBERA is IERC20 {
+    function deposit() external payable;
+}
 interface IVault {
     function swap(
-        IVault.SingleSwap calldata singleSwap,
-        IVault.FundManagement calldata funds,
+        SingleSwap memory singleSwap,
+        FundManagement memory funds,
         uint256 limit,
         uint256 deadline
-    ) external returns (uint256);
+    ) external payable returns (uint256);
 
     struct SingleSwap {
         bytes32 poolId;
@@ -32,38 +34,50 @@ interface IVault {
 
 contract TokenSwapper {
     IVault public vault;
+    address public immutable WETH;
 
-    constructor(address _vaultAddress) {
+    constructor(address _vaultAddress, address _WETH) {
         vault = IVault(_vaultAddress);
+        WETH = _WETH;
     }
 
-    function swapTokens(
-        bytes32 poolId,
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn,
-        uint256 minAmountOut,
-        address recipient
-    ) external {
+function swapTokens(
+    bytes32 poolId,
+    address tokenIn,
+    address tokenOut,
+    uint256 amountIn,
+    uint256 minAmountOut
+) external payable {
+    bool isETH = tokenIn == address(0);
+
+    if (isETH) {
+        require(msg.value == amountIn, "Incorrect ETH amount sent");
+        IWBERA(WETH).deposit{value: msg.value}(); 
+        IWBERA(WETH).approve(address(vault), amountIn);
+    } else {
         IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
         IERC20(tokenIn).approve(address(vault), amountIn);
-
-        IVault.SingleSwap memory singleSwap = IVault.SingleSwap({
-            poolId: poolId,
-            kind: IVault.SwapKind.GIVEN_IN,
-            assetIn: IERC20(tokenIn),
-            assetOut: IERC20(tokenOut),
-            amount: amountIn,
-            userData: ""
-        });
-
-        IVault.FundManagement memory funds = IVault.FundManagement({
-            sender: address(this),
-            fromInternalBalance: false,
-            recipient: payable(recipient),
-            toInternalBalance: false
-        });
-
-        vault.swap(singleSwap, funds, minAmountOut, block.timestamp + 10 minutes);
     }
+
+    IVault.SingleSwap memory singleSwap = IVault.SingleSwap({
+        poolId: poolId,
+        kind: IVault.SwapKind.GIVEN_IN,
+        assetIn: IERC20(isETH ? WETH : tokenIn),
+        assetOut: IERC20(tokenOut),
+        amount: amountIn,
+        userData: ""
+    });
+
+    IVault.FundManagement memory funds = IVault.FundManagement({
+        sender: address(this),
+        fromInternalBalance: false,
+        recipient: payable(msg.sender),
+        toInternalBalance: false
+    });
+
+    vault.swap(singleSwap,funds,minAmountOut,block.timestamp + 10 minutes);
+}
+
+
+    receive() external payable {}
 }
